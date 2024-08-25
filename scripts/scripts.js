0c052515f323aef93ff402bb781f647786c4ba48
KARIB9 <suleymanov.karib@gmail.com>
2024-08-25 19:17:56 +0300

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const video = document.getElementById('boomerang-video')
+    const btmPauseBlock = document.querySelector('.video-container')
+    let reverse = false
+    let interval
+    const frameTime = 0.030
+
+    function playForward() {
+        reverse = false
+        video.play()
+    }
+
+    function playReverse() {
+        reverse = true
+        clearInterval(interval)
+
+        interval = setInterval(() => {
+            if(video.currentTime <= 0) {
+                clearInterval(interval)
+                playForward()
+            } else {
+                video.currentTime -= frameTime
+            }
+        }, frameTime * 1000);
+    }
+    
+    video.addEventListener('timeupdate', () => {
+        if(!reverse && video.currentTime >= video.duration - frameTime) {
+            playReverse()
+        }
+    })
+
+    video.play()
+})
+
